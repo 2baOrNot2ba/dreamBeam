77ea6ed1ade7adb8990a2372f2f60c01c2531770
Tobia Carozzi <tobia@chalmers.se>
2019-08-20 14:32:32 +0200

    Some PEP8 changes.

--- dreambeam/telescopes/LOFAR/telwizhelper.py
+++ dreambeam/telescopes/LOFAR/telwizhelper.py
@@ -6,7 +6,7 @@ from antpat.dualpolelem import DualPolElem
 from antpat.reps.hamaker import HamakerPolarimeter
 from dreambeam.telescopes import rt
 from dreambeam.telescopes.geometry_ingest import readarrcfg, readalignment
-from dreambeam.telescopes.LOFAR.feeds import LOFAR_LBA_stn, LOFAR_HBA_stn, LOFAR_LHBA_stn
+from dreambeam.telescopes.LOFAR.feeds import LOFAR_LHBA_stn
 
 TELESCOPE_NAME = 'LOFAR'
 NR_POLS = 2
@@ -31,15 +31,15 @@ DP_BAFILES = {'LBA': DP_LBA_FILE_DEF, 'HBA': DP_HBA_FILE_DEF}
 
 bands = DP_BAFILES.keys()
 
-#Start up a telescope wizard:
-tw = rt.TelescopesWiz()
+# Start up a telescope wizard:
+TW = rt.TelescopesWiz()
 
 
 def read_LOFAR_HAcc(coefsccfilename):
     """Read Hamaker-Arts coefficients from c++ header files used in the
     "lofar_element_response" code developed at ASTRON for LOFAR. It contains
-    LOFAR specific constructs such as reference to "lba" and "hba", so it is not
-    suitable for other projects.
+    LOFAR specific constructs such as reference to "lba" and "hba", so it is
+    not suitable for other projects.
     """
     re_fcenter = r'[lh]ba_freq_center\s*=\s*(?P<centerstr>.*);'
     re_frange  = r'[lh]ba_freq_range\s*=\s*(?P<rangestr>.*);'
@@ -53,7 +53,8 @@ def read_LOFAR_HAcc(coefsccfilename):
     searchres = re.search(re_frange, coefsfile_content)
     freq_range = float(searchres.group('rangestr'))
     searchres = re.search(re_shape, coefsfile_content)
-    lstshp = [int(lstshpel) for lstshpel in searchres.group('lstshp').split(',')]
+    lstshp = [int(lstshpel) for lstshpel in
+              searchres.group('lstshp').split(',')]
     lstshp.append(NR_POLS)
     searchres = re.search(re_hl_ba_coeffs_lst, coefsfile_content, re.M)
     HAcoefversion = searchres.group('version')
@@ -63,13 +64,13 @@ def read_LOFAR_HAcc(coefsccfilename):
     cmplx_lst = []
     for reimstr in lstofCmpl:
         reimstrs = reimstr.split(',')
-        cmplx_lst.append( complex(float(reimstrs[0]), float(reimstrs[1])) )
+        cmplx_lst.append(complex(float(reimstrs[0]), float(reimstrs[1])))
     coefs = numpy.reshape(numpy.array(cmplx_lst), lstshp)
-    #The coefficients are order now as follows:
-    #coefs[k,theta,freq,spherical-component].shape == (2,5,5,2)
-    artsdata={'coefs': coefs, 'HAcoefversion': HAcoefversion,
-              'HAcoefband': HAcoefband, 'HAcoefnrelem': HAcoefnrelem,
-              'freq_center': freq_center, 'freq_range': freq_range}
+    # The coefficients are order now as follows:
+    #   coefs[k,theta,freq,spherical-component].shape == (2,5,5,2)
+    artsdata = {'coefs': coefs, 'HAcoefversion': HAcoefversion,
+                'HAcoefband': HAcoefband, 'HAcoefnrelem': HAcoefnrelem,
+                'freq_center': freq_center, 'freq_range': freq_range}
     return artsdata
 
 
@@ -91,10 +92,9 @@ def convHA2DPE(inp_HA_file, out_DP_file):
 
 
 def gen_antmodelfiles(inpfileL=LOFAR_HA_DATADIR+'DefaultCoeffLBA.cc',
-                       inpfileH=LOFAR_HA_DATADIR+'DefaultCoeffHBA.cc',
-                       outfileL=HA_LBA_FILE_DEF,
-                       outfileH=HA_HBA_FILE_DEF
-                       ):
+                      inpfileH=LOFAR_HA_DATADIR+'DefaultCoeffHBA.cc',
+                      outfileL=HA_LBA_FILE_DEF,
+                      outfileH=HA_HBA_FILE_DEF):
     """A convenience function to produce the pickled 'artsdata' for the default
     LOFAR model data stored in the 'lofar_elem_resp' packages c++ header files.
     Also adds nominal LOFAR frequency channels."""
@@ -108,7 +108,8 @@ def gen_antmodelfiles(inpfileL=LOFAR_HA_DATADIR+'DefaultCoeffLBA.cc',
     convLOFARcc2HA(inpfileL, outfileL, channels)
     inpfileL = outfileL
     convHA2DPE(inpfileL, DP_LBA_FILE_DEF)
-    channels = numpy.linspace(SAMPFREQ, 3*SAMPFREQ, 2*NR_CHANNELS, endpoint=False)
+    channels = numpy.linspace(SAMPFREQ, 3*SAMPFREQ, 2*NR_CHANNELS,
+                              endpoint=False)
     convLOFARcc2HA(inpfileH, outfileH, channels)
     inpfileH = outfileH
     convHA2DPE(inpfileH, DP_HBA_FILE_DEF)
@@ -119,27 +120,24 @@ def save_telescopeband(band, antmodel='Hamaker'):
     one file."""
     assert band in bands, ("Error: {} is not one of the available bands.\n"
                            "(Available bands are: {})").format(band, bands)
-    assert antmodel in ANTMODELS, ("Error: {} is not one of the available models.\n"
-                                  "Available models are: {})").format(antmodel, ANTMODELS)
-    print("Generating '{}' beam-model for the band {} of the {} telescope with stations:".format(
-                                                   antmodel, band, TELESCOPE_NAME))
+    assert antmodel in ANTMODELS, (
+        """Error: {} is not one of the available models.
+        Available models are: {}""").format(antmodel, ANTMODELS)
+    print("""Generating '{}' beam-model for the band {} of the {} telescope
+          with stations:""".format(antmodel, band, TELESCOPE_NAME))
     # Create telescope-bands metadata:
     telescope = {'Name': TELESCOPE_NAME, 'Band': band, 'Beam-model': antmodel}
     #   * Create station's antenna model
     DP_BAfile = DP_BAFILES[band]
     if antmodel == 'Hamaker':
-        #This is an example of dual-pol element built from a monolithic
-        #Jones representation.
+        # This is an example of dual-pol element built from a monolithic
+        # Jones representation.
         stnDPolel = pickle.load(open(DP_BAfile, 'rb'))
-    #Rotate 45 degrees since LOFAR elements are 45 degrees to meridian:
+    # Rotate 45 degrees since LOFAR elements are 45 degrees to meridian:
     stnDPolel.rotateframe(POLCRDROT)
 
-    #Create telescope_band_station metadata:
+    # Create telescope_band_station metadata:
     telescope['Station'] = {}
-    if band == BANDS[0]:
-        LOFAR_BA_stn = LOFAR_LBA_stn
-    else:
-        LOFAR_BA_stn = LOFAR_HBA_stn
     LOFAR_BA_stn = LOFAR_LHBA_stn
     x, y, z, diam, stnIds = readarrcfg(TELESCOPE_NAME, band)
 
@@ -155,8 +153,9 @@ def save_telescopeband(band, antmodel='Hamaker'):
         stnbnd = LOFAR_BA_stn(stnPos, stnRot)
         stnbnd.feed_pat = stnDPolel
         telescope['Station'][stnId] = stnbnd
-    teldatdir, saveName = tw.telbndmdl2dirfile(TELESCOPE_NAME, band, antmodel)
-    pickle.dump(telescope, open('/'.join((teldatdir, saveName)), 'wb'), PICKLE_PROTO)
+    teldatdir, saveName = TW.telbndmdl2dirfile(TELESCOPE_NAME, band, antmodel)
+    pickle.dump(telescope, open('/'.join((teldatdir, saveName)), 'wb'),
+                PICKLE_PROTO)
     print("Saved '"+saveName+"' in "+teldatdir)
 
 
@@ -166,9 +165,7 @@ if __name__ == "__main__":
     $ python telwizhelper.py
     """
     gen_antmodelfiles()
-    #stnlst = list_stations(TELESCOPE_NAME)
     antmodel = 'Hamaker'
     for band in BANDS:
-        #save_telescopeband(band, stnlst, antmodel)
         save_telescopeband(band, antmodel)
     print("Completed setup.")
