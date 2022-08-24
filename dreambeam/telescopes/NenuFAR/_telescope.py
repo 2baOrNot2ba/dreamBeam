41aa0c2574bf26f72e538e5bc6f2c9261d0be0b2
Tobia Carozzi <tobia@chalmers.se>
2022-08-24 13:14:29 +0200

    Changed: X/Y orientation of NenuFAR flipped wrt LOFAR

--- dreambeam/telescopes/NenuFAR/_telescope.py
+++ dreambeam/telescopes/NenuFAR/_telescope.py
@@ -5,9 +5,11 @@ from dreambeam.telescopes.mounts import MountedFeedFixed
 from dreambeam.telescopes.rt import TelescopePlugin
 
 OOSR2 = 1./numpy.sqrt(2)
-# Rotation of LOFAR antennas from build frame to station frame.
-# It takes x/y and directed dipoles and places them along (-1,-1)/(+1,-1) resp.
-POLCRDROT = numpy.array([[-OOSR2, +OOSR2,  0.],
+# Rotation of NenuFAR antennas from build frame to station frame.
+# It takes x/y and directed dipoles and places them along (+1,-1)/(-1,-1) resp.
+# Note that this is flipped w.r.t. LOFAR alignment i.e. X/Y <-> Y/X.
+# Reference: Communication with NenuFAR people; unfortunately not documented. 
+POLCRDROT = numpy.array([[+OOSR2, -OOSR2,  0.],
                          [-OOSR2, -OOSR2,  0.],
                          [    0.,     0.,  1.]])
 TELESCOPE_NAME = basename(dirname(abspath(__file__)))
