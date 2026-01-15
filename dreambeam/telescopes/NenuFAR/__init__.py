07ac61f2373489890d0cb31ebd0ea439a306e176
Tobia Carozzi <tobia@chalmers.se>
2026-01-15 13:55:29 +0100

    Bumped: NenuFAR plugin to v0.2

--- dreambeam/telescopes/NenuFAR/__init__.py
+++ dreambeam/telescopes/NenuFAR/__init__.py
@@ -1,2 +1,9 @@
-"""Plugin package for NenuFAR beam-models"""
-__version__ = '0.1'
+"""\
+Plugin package for NenuFAR beam-models
+"""
+# Note on provinence: Julien Girard asked me to work on this plugin back in
+#    2019. He provided NEC4 antenna pattern (E-field direction grids) files
+#    created by Didier Charrier. He asked me to make NenuFAR plugin as similar
+#    as possible to the analogous plugin for LOFAR, which meant using the 
+#    Hamakers model, and a normalization of 1 for the maximum Jones.
+__version__ = '0.2'
